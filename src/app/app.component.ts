ec8766e9bd57af95fc019d7acdb0071a9c102690
duc-nhat38 <phamdinhduc02071998@gmail.com>
2020-09-04 11:24:45 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
-
+interface Article {
+  title: string;
+  url: string;
+  like: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'hackernews';
+export class AppComponent{
+  article:Article = {
+    title: '',
+    url: '',
+    like: 0
+  };
+  articles: Array<Article> = [
+    {
+      title: 'The Evolution of Async JavaScript: From Callbacks, to Promises, to Async/Await',
+      url: 'https://medium.freecodecamp.org/the-evolution-of-async-javascript-from-callbacks-to-promises-to-async-await-e73b047f2f40',
+      like: 0
+    },
+    {
+      title: 'Game of Life',
+      url: 'https://thefullsnack.com/posts/game-of-life.html',
+      like: 0
+    },
+    {
+      title: 'Nguyên tắc thiết kế REST API',
+      url: 'https://medium.com/eway/nguyên-tắc-thiết-kế-rest-api-23add16968d7',
+      like: 0
+    },
+    {
+      title: 'Why You Only Need to Test with 5 Users',
+      url: 'https://www.nngroup.com/articles/why-you-only-need-to-test-with-5-users/',
+      like: 0
+    },
+    {
+      title: 'Let’s Build A Web Server. Part 1.',
+      url: 'https://ruslanspivak.com/lsbaws-part1/',
+      like: 0
+    }
+  ];
+  addArticle(){
+    this.articles.push(this.article);
+    return this.article = {
+      title: '',
+      url: '',
+      like: 0
+    }
+  }
+
+  isLike(i){
+    this.articles[i].like ++;
+  }
 }
